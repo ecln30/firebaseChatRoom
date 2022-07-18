49261b8e480d6ecf471688fc9e6716a6a6a41fb7
ecln30 <ecln30@gmail.com>
2022-07-18 11:52:01 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, {useState, useRef} from "react"
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from "react-firebase-hooks/firestore"
+import { db, auth,  signInWithGoogle, signOut } from "./firebase.js"
+import { query, getDocs, addDoc, orderBy, limit, collection, serverTimestamp } from "firebase/firestore"
+import send from "./send.png"
+
 
 function App() {
+  const [user] = useAuthState(auth)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <h1>⚛️🔥💬</h1>
+        <SignOut />
       </header>
+      <section>
+        { user ? <ChatRoom /> : <SignIn /> }
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+   return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+     
+    </>
+   )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+   
+   const [value, setValue] = useState('')
+   const dummy = useRef()
+   const messageRef = collection(db, "messages")
+   const q = query(messageRef, orderBy("createdAt"), limit(25))
+   const [messages] = useCollectionData(q, { idField: 'id' })
+    console.log(messages)
+   
+   const sendMessage = async (e) => {
+      e.preventDefault()
+      const { uid, photoURL} = auth.currentUser
+       console.log(messageRef)
+      await addDoc(messageRef, {
+         text: value,
+         createdAt:  serverTimestamp(), 
+         uid,
+         photoURL
+      })
+
+      setValue('');
+      dummy.current.scrollIntoView({ behavior: 'smooth' });
+   }
+   return (
+    <>
+
+      <main>
+       {messages && messages.map(msg => <ChatMessage
+         key={msg.id}
+         message={msg}/>)}
+
+         <span ref={dummy}></span>
+      </main>
+     
+      <form onSubmit={sendMessage}>
+
+       <input value={value} onChange={ e => setValue(e.target.value)} />
+       <button type="submit"  disabled={!value}>
+        <img src={send} alt="" />
+       </button>
+     </form>
+      
+    </>
+   )
+}
+
+function ChatMessage({message}) {
+   const { text, uid, photoURL} = message
+   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received'
+   return (
+      <div className={`message ${messageClass}`}>
+          <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+         <p> {text} </p>
+      </div>
+   )
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,120 @@
+import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnJtFIoI7JAtoWBg_BNJc2A3nTiWvMD-E",
+  authDomain: "firechat-27fc6.firebaseapp.com",
+  projectId: "firechat-27fc6",
+  storageBucket: "firechat-27fc6.appspot.com",
+  messagingSenderId: "544169953704",
+  appId: "1:544169953704:web:3fdbf1e0d7a9dde658759d",
+  measurementId: "G-RK29JFRPE2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "user"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "user"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+ 
+};
+const logInEnP = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const registerEnP = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+    alert("registered Sucessfully")
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const logout = () => {
+  signOut(auth);
+};
+
+export {
+  db,
+  auth,
+  logout,
+  logInEnP,
+  registerEnP,
+  signInWithGoogle,
+  sendPasswordReset,
+  onAuthStateChanged,
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
